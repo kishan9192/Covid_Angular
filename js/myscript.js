05b2167aa8416e2193d38f1b752b151b7a0fd09b
kishan9192 <mishra.kishan1@gmail.com>
2021-01-14 23:27:35 +0530

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,46 @@
+let app = angular.module("MyApp", []);
+
+const URL = "https://covid19.mathdro.id/api"
+
+
+app.controller("MyCtrl", ($scope, $http) => {
+
+    //this is the controller
+    $scope.title = "Stay Home Stay Safe";
+
+    // Button that changes title
+    $scope.changeValue = ()=> {
+        $scope.title = "दो गज की दूरी और मास्क ही है जरूरी!";
+    };
+    
+
+    // Function to get the data from the server
+    $http.get(URL).then( (response) => {
+        // success coding
+        console.log("Inside success");
+        console.log(response.data);
+
+        // using $scope so that we can access the data in our index.html
+        $scope.all_data = response.data;
+    } , (error) => {
+        console.log(error);
+    });
+
+    $scope.getCountryData = ()=> {
+        let country = $scope.c;
+        if (country == "") {
+            return;
+        }
+    };
+
+    // here last me "country is the varaible that has been defined above"
+    $http.get(`${URL}/countries/{country}`)
+    .then((response)=>{
+        console.log(response.data);
+        // This variable c_data has been made so that we can access the data of a country in our index.html file
+        $scope.c_data = response.data; 
+    }, (error)=>{
+        console.log(error);
+    })
+
+});
