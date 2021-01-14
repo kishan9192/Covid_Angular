f07f12b5245cd78ed536ecebd090f9b6bf85f839
kishan9192 <mishra.kishan1@gmail.com>
2021-01-15 01:17:13 +0530

    added image

--- js/myscript.js
+++ js/myscript.js
@@ -14,7 +14,7 @@ app.controller("MyCtrl", ($scope, $http) => {
     };
     
 
-    // Function to get the data from the server
+    // Function to get the data from the server URL
     $http.get(URL).then( (response) => {
         // success coding
         console.log("Inside success");
@@ -22,25 +22,30 @@ app.controller("MyCtrl", ($scope, $http) => {
 
         // using $scope so that we can access the data in our index.html
         $scope.all_data = response.data;
+
     } , (error) => {
         console.log(error);
     });
 
+    
     $scope.getCountryData = ()=> {
         let country = $scope.c;
         if (country == "") {
+            // This disappears the row when everything from the input is removed
+            $scope.c_data = undefined;
             return;
         }
-    };
 
     // here last me "country is the varaible that has been defined above"
-    $http.get(`${URL}/countries/{country}`)
-    .then((response)=>{
-        console.log(response.data);
-        // This variable c_data has been made so that we can access the data of a country in our index.html file
-        $scope.c_data = response.data; 
-    }, (error)=>{
-        console.log(error);
-    })
+        $http.get(`${URL}/countries/${country}`)
+        .then((response)=>{
+            console.log(response.data);
 
+            // This variable c_data has been made so that we can access the data of a country in our index.html file
+            $scope.c_data = response.data; 
+
+        }, (error)=>{
+            console.log(error);
+        })
+    };
 });
